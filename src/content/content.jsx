9125cd2c7661b667b3dc94b25c780ead6cca9614
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-04-04 10:24:00 -0300

    fix: BlogPosts

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -444,13 +444,15 @@ const content = {
     blog: {
       miniTag: "BLOG",
       title: "Informação jurídica ao seu alcance",
-      subtitle: "Acompanhe conteúdos exclusivos para manter-se sempre informado.",
+      subtitle:
+        "Acompanhe conteúdos exclusivos para manter-se sempre informado.",
       img: imgSteps,
       blogApiEndpoint:
-        "https://public-api.wordpress.com/rest/v1.1/sites/blogtestepaper.wordpress.com/posts/",
+        "https://public-api.wordpress.com/rest/v1.1/sites/luishenriquecabraladv.wordpress.com/posts/",
+
       labelCards: "Ver mais",
       label: "Clique aqui para ver todas as matérias",
-      blogLink: "https://blogtestepaper.wordpress.com/",
+      blogLink: "https://luishenriquecabraladv.wordpress.com/",
     },
     faq: {
       miniTag: "TIRE SUAS DÚVIDAS",
